20eef74f7249fca2dd178afb1acb8f3e8f98b06a
Luke Fox <l.fox9@nuigalway.ie>
2019-09-27 18:13:59 +0100

    created basic device list screen and added a back button to both secondary layouts

--- app/src/main/java/android/lukefox/bluetooth_gps/MainActivity.java
+++ app/src/main/java/android/lukefox/bluetooth_gps/MainActivity.java
@@ -1,18 +1,12 @@
 package android.lukefox.bluetooth_gps;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button mapButton; //
-    private Button devicesButton; //
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     public void onDeviceClick(View v) {
 
         Intent intent = new Intent(getBaseContext(),
-                MapsActivity.class);
+                ListDevicesActivity.class);
         startActivity(intent); // startActivity allow you to move
     }
 }
