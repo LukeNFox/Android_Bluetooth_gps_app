64d8ed2fc260eda5e1812e45af1479e3f55eb6b9
Luke Fox <l.fox9@nuigalway.ie>
2019-12-28 20:16:24 +0000

    requesting bluetooth permissions on startup

--- app/src/main/java/android/lukefox/bluetooth_gps/MainActivity.java
+++ app/src/main/java/android/lukefox/bluetooth_gps/MainActivity.java
@@ -1,12 +1,17 @@
 package android.lukefox.bluetooth_gps;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public final int REQUEST_ENABLE_BT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);// activity_main is corresponding XML file
 
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter != null ) {
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                Toast.makeText(this, "Bluetooth Connected", Toast.LENGTH_SHORT).show();
+            }
+        } else{
+            Toast.makeText(this, "Bluetooth Unavailable on this device", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onMapClick(View v) {
