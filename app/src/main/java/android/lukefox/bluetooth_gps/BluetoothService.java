664aff2e4b4fa59a3bdcac0c2f2702f4e9f5765b
Luke Fox <l.fox9@nuigalway.ie>
2019-12-28 20:12:13 +0000

    bluetooth service boiler plate code

---
+++ app/src/main/java/android/lukefox/bluetooth_gps/BluetoothService.java
@@ -0,0 +1,23 @@
+package android.lukefox.bluetooth_gps;
+
+import android.app.IntentService;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+
+
+public class BluetoothService extends IntentService {
+
+    public BluetoothService() {
+
+        super("BluetoothService");
+
+    }
+
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+
+    }
+}
